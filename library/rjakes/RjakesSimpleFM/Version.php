a758f9163fa80a2c3c4cb4b9b54fda6aaa6a63be
Roger Jacques <roger@rjakes.com>
2014-08-01 12:50:22 -0700

    correct semantic version number

--- library/rjakes/RjakesSimpleFM/Version.php
+++ library/rjakes/RjakesSimpleFM/Version.php
@@ -3,13 +3,13 @@
  * This source file is subject to the MIT license that is bundled with this package in the file LICENSE.txt.
  *
  * @package   rjakes\RjakesSimpleFM
- * @copyright Copyright (c) 20012-2015 Roger Jacques Consulting (RJAKES LLC). (http://www.rjakes.com)
- * @author    roger@rjakes.com
+ * @copyright Copyright (c) 20012-2015 ROger Jacques Consulting (RJAKES LLC). (http://www.rjakes.com)
+ * @author    jsmall@soliantconsulting.com
  */
 
 namespace rjakes\RjakesSimpleFM;
 
 final class Version
 {
-    const VERSION = '0.0.0alpha2';
+    const VERSION = '1.0.0alpha2';
 }
